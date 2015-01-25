a1af70de71c6616c9b544a55335f71549b6c3d76
cole <cole@mbuntu.com>
2015-01-25 22:12:38 +0800

    version 0.0.1

---
+++ pyfetion/web.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding:utf-8
+__author__ = 'Jayvic'
+__date__ = '14-7-20'
+
+import urllib
+import urllib2
+import cookielib
+
+
+class _Web(object):
+    """Abstract class to open URLs with cookie."""
+    def open(self, url, data=None, method='POST'):
+        """Return string from the specified URL."""
+
+
+class UrllibWeb(_Web):
+    """Class to open URLs with cookie by urllib2."""
+    def __init__(self):
+        """Init opener with cookie processor."""
+        try:
+            cj = cookielib.CookieJar()
+            self._opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+            # self._opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36 QQBrowser/2.4.2181.400')]
+        except Exception, error:
+            print(error)
+
+    def open(self, url, data=None, method='POST'):
+        """Return string from the specified URL."""
+        try:
+            if method.upper() == 'POST':
+                if data is None:
+                    return self._opener.open(url).read()
+                else:
+                    return self._opener.open(url, urllib.urlencode(data)).read()
+            elif method.upper() == 'GET':
+                return self._opener.open(url + '?' + urllib.urlencode(data)).read()
+            else:
+                raise ValueError('No such method named %s' % method)
+        except Exception, error:
+            print(error)
---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+MODULE_NAME = 'pyfetion'
+VERSION = '0.0.1'
+AUTHOR = 'Cole Smith'
+AUTHOR_EMAIL = 'uniquecolesmith@gmail.com'
+URL = ''
+
+PACKAGES = [
+    'pyfetion',
+]
+
+REQUIRES = []
+
+try:
+    with open('README.md', 'r') as fp:
+        readme = fp.read()
+except:
+    readme = ''
+
+try:
+    with open('HISTORY.md', 'r') as fp:
+        history = fp.read()
+except:
+    history = ''
+
+setup(
+    name = MODULE_NAME,
+    version = VERSION, 
+    description = 'Simple Fetion Message',
+    long_description = readme + '\n' + history,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    packages = PACKAGES,
+    package_dir = {'pyfetion': 'pyfetion'},
+    include_package_data = True,
+    install_requires = REQUIRES,
+    license = 'Apache 2.0',
+    zip_safe = False,
+    classifiers = (
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+)
